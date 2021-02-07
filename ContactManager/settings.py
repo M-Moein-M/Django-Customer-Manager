e0b0f8448eb72d1c14b810eaf3fb47fb4f7fdd00
Moein <mohammadmoein.moradi@gmail.com>
2021-02-07 13:55:56 +0330

    chore: add static folder

--- ContactManager/settings.py
+++ ContactManager/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
