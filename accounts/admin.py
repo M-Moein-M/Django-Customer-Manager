cdc9c9a915c7f9f9675cd671dc72772b162e5d24
Moein <mohammadmoein.moradi@gmail.com>
2021-02-08 09:50:41 +0330

    chore: add Product model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 
-from .models import Customer
+from .models import *
 admin.site.register(Customer)
+admin.site.register(Product)
--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,16 @@ class Customer(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class Product(models.Model):
+	CATEGORY = (
+		('Indoor', 'Indoor'),
+		('Out Door', 'Out Door'),
+	)
+
+	name = models.CharField(max_length=100, null=True)
+	price = models.FloatField(null=True)
+	category = models.CharField(max_length=100, null=True, choices=CATEGORY)
+	description = models.CharField(max_length=1023, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
