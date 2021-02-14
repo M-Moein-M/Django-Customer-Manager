c1452d32b0dd3b0cfc8bc86d29c09ffab27ecf26
Moein <mohammadmoein.moradi@gmail.com>
2021-02-14 20:11:29 +0330

    feat: render status on dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,19 @@ from .models import *
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
     context = {
         'orders': orders,
-        'customers': customers
+        'customers': customers,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending
     }
 
     return render(request, 'accounts/dashboard.html', context)
