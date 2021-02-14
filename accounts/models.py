d484ea3e475f6da938f093b22328167254d63d44
Moein <mohammadmoein.moradi@gmail.com>
2021-02-14 19:50:30 +0330

    feat: render data to products page

--- accounts/models.py
+++ accounts/models.py
@@ -46,4 +46,3 @@ class Order(models.Model):
 	product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=100, choices=STATUS)
-
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import *
 
 def home(request):
     return render(request, 'accounts/dashboard.html')
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 
 def customer(request):
     return render(request, 'accounts/customer.html')
-
