41fa38f1bdd2d1ebd7d898659e1fffe6acf9ad0e
Moein <mohammadmoein.moradi@gmail.com>
2021-02-21 17:45:43 +0330

    feat: remove create-order link from dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('products/', views.products, name='products'),
     path('customer/<str:customer_id>', views.customer, name='customer'),
 
-    path('create_order', views.createOrder, name='create_order'),
+    path('create_order/<str:pk>', views.createOrder, name='create_order'),
     path('update_order/<str:pk>', views.updateOrder, name='update_order'),
     path('delete_order/<str:pk>', views.deleteOrder, name='delete_order'),
 ]
