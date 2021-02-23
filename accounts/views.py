5f48060c0dca3eb0f79e6a14f7e4b02878e9c829
Moein <mohammadmoein.moradi@gmail.com>
2021-02-23 16:23:10 +0330

    feat: add login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,22 @@ from .forms import OrderForm, CreateUserForm
 from django.forms import inlineformset_factory
 from .filters import OrderFilter
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
     context = {}
     return render(request, 'accounts/login.html', context)
 
