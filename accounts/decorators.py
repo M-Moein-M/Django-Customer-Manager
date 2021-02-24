d56f3816d5c5f366261df6c8083dc30f3083efcc
Moein <mohammadmoein.moradi@gmail.com>
2021-02-24 15:57:49 +0330

    feat: add allowed_user decorator

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -10,3 +10,20 @@ def unauthenticated_user(view_func):
 			return view_func(request, *args, **kwargs)
 
 	return wrapper_func
+
+
+def allowed_user(allowed_roles=[]):
+	def decorator(view_func):
+		def wrapper_func(request, *args, **kwargs):
+			group = None
+			if request.user.groups.exists():
+				group = request.user.groups.all()[0].name
+
+			if group in allowed_roles:
+				return view_func(request, *args, **kwargs)
+			else:
+				return HttpResponse('You Are Not Authorized To View This Page')
+
+		return wrapper_func
+
+	return decorator
