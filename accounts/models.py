dce81411f0aab6baa71b84395af94fa25814ddbc
Moein <mohammadmoein.moradi@gmail.com>
2021-02-26 16:23:51 +0330

    feat: set user's default picture

--- accounts/models.py
+++ accounts/models.py
@@ -7,11 +7,11 @@ class Customer(models.Model):
 	name = models.CharField(max_length=100, null=True)
 	phone = models.CharField(max_length=100, null=True)
 	email = models.CharField(max_length=200, null=True)
-	profile_pic = models.ImageField(null=True, blank=True)
+	profile_pic = models.ImageField(default='/DEFAULTPROFILEPIC_VIwfoSMcf7IB2kdI4yKa.svg', null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 	def __str__(self):
-		return self.name
+		return self.name or '**UNKNOWN**'
 
 
 class Tag(models.Model):
