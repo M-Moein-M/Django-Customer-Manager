e18dfc3ad4e6d55141b0d929f31e2970ef99de2d
Moein <mohammadmoein.moradi@gmail.com>
2021-03-06 13:39:39 +0330

    feat: save new order with quantity

--- accounts/utils/order.py
+++ accounts/utils/order.py
@@ -1,13 +1,25 @@
 from ..models import Customer, Product, Order
+from ..forms import NewOrderForm
 
 
 class SaveNewOrder:
 	def __init__(self, request, pk):
 		customer_id = request.user.customer.id
+		self.quantity = self.get_order_quantity_from_form_data(request)
 		self.customer = Customer.objects.get(id=customer_id)
 		self.product = Product.objects.get(id=pk)
 
 	def save_order(self):
-		Order(customer=self.customer,
-			  product=self.product,
-			  status='Pending').save()
+		order = self.get_order_object()
+		order.save()
+
+	def get_order_quantity_from_form_data(self, request):
+		form = NewOrderForm(request.POST)
+		if form.is_valid():
+			return form.cleaned_data.get('quantity')
+
+	def get_order_object(self):
+		return Order(customer=self.customer,
+					 product=self.product,
+					 status='Pending',
+					 quantity=self.quantity)
