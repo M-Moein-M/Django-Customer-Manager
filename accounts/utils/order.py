7946e3d2576649000e49e19397c4e9140afbfd8f
Moein <mohammadmoein.moradi@gmail.com>
2021-03-07 10:01:27 +0330

    chore: inc order quantity if specific product-order exists

--- accounts/utils/order.py
+++ accounts/utils/order.py
@@ -1,4 +1,5 @@
 from ..models import Customer, Product, Order
+from django.core.exceptions import ObjectDoesNotExist
 from ..forms import NewOrderForm
 
 
@@ -10,15 +11,28 @@ class SaveNewOrder:
 		self.product = Product.objects.get(id=pk)
 
 	def save_order(self):
-		order = self.get_order_object()
+		order = self.create_order_or_increment_quantity()
 		order.save()
+
+	def create_order_or_increment_quantity(self):
+		try:
+			return self.try_to_increment_quantity()
+		except Order.DoesNotExist:
+			return self.create_new_order_object()
+
+	def try_to_increment_quantity(self):
+		order = Order.objects.get(customer=self.customer.id,
+								  product=self.product.id,
+								  status='Pending')
+		order.quantity += self.quantity
+		return order
 
 	def get_order_quantity_from_form_data(self, request):
 		form = NewOrderForm(request.POST)
 		if form.is_valid():
 			return form.cleaned_data.get('quantity')
 
-	def get_order_object(self):
+	def create_new_order_object(self):
 		return Order(customer=self.customer,
 					 product=self.product,
 					 status='Pending',
