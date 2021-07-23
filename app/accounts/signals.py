a92fb4ee0003adcc08029fc2cbdd678a26b260ba
Moein <mohammadmoein.moradi@gmail.com>
2021-07-23 09:16:20 +0430

    fix adding superuser to customer group

--- app/accounts/signals.py
+++ app/accounts/signals.py
@@ -5,9 +5,15 @@ from .models import Customer
 
 def customer_profile(sender, instance, created, **kwargs):
 	if created:
-		group = Group.objects.get(name='customer')
-		instance.groups.add(group)
-		Customer.objects.create(user=instance, name=instance.username)
+		if instance.is_staff:
+			add_group(instance, 'admin')
+		else:
+			add_group(instance, 'customer')
 
+
+def add_group(instance, group_name):
+	group = Group.objects.get(name=group_name)
+	instance.groups.add(group)
+	Customer.objects.create(user=instance, name=instance.username)
 
 post_save.connect(customer_profile, sender=User)
